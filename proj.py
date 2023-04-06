96847568b698f5d467a420cc72dc0fd60865b712
Michał Ziober <michalziober08@gmail.com>
2023-04-06 15:46:01 +0200

    view from front

---
+++ proj.py
@@ -0,0 +1,52 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import coordinates, edges
+
+def Cube(ver, edges):
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(ver[vertex])
+    glEnd()
+
+
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0, -5)
+
+    i = 0.0
+    flag = True
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+
+        if (i > 1):
+            flag = False
+        elif (i < 0):
+            flag = True
+
+        if(flag):
+            i += 0.01
+        else:
+            i -= 0.01
+
+        # glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube(coordinates.verticies, edges.edges)
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
